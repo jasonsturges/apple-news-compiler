d60cf1ebf5b517e29fdbb6541abbe12ae8c2a19d
Jason Sturges <jason@jasonsturges.com>
2019-03-25 00:02:53 -0500

    Inline compiler

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * apple-news-compiler <https://github.com/jasonsturges/apple-news-compiler>
+ *
+ * Copyright (c) 2019 Jason Sturges
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+const article = require("./lib/article")
+
+module.exports = {
+  article
+};
